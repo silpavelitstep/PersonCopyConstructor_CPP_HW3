e8372f1dfc92a4c8a98bc70158d91304d5c5b4d0
silpavel@outlook.com <silpavel@outlook.com>
2019-05-08 15:12:24 +0300

    testing and

--- PersonCopyConstructor_CPP_HW3/test_set_get.cpp
+++ PersonCopyConstructor_CPP_HW3/test_set_get.cpp
@@ -4,20 +4,37 @@ using namespace std;
 #include "test_set_get.h"
 void Person_test::exeAllTest() {
 	cout << "\n\nExecute all tests: \n";
-	setExist();
-	getExist();
-	setTrueBackValue();
-	getTrueBackValue();
+	//next testing
+	test();
+	
+	//end testing
+	
 	cout << "Testing end, resault: ";
-	if (set_exist&&get_exist&&set_true_back_value&&set_true_back_value)
+	if (exist&&true_back_value)
 		cout << "PASS\n";
 	else
 		cout << "FAIL\n";
 }
-void Person_test::setExist() {}
-void Person_test::getExist() {}
-void Person_test::setTrueBackValue() {}
-void Person_test::getTrueBackValue() {}
+void Person_test::test() {
+	Person *p = new Person("A", 1, 'c', 8);
+	//check exist getters
+	exist = !strcmp(p->getName(), "A") && p->getYearOfBirth() == 1
+		&& p->getSex() == 'c' && p->getPhoneNumber() == 8;
+	//check exist setter and true back value
+	p->setName("B");
+	p->setYearOfBirth(1925);
+	p->setSex('x');
+	p->setPhoneNumber(123456);
+
+	true_back_value=exist = !strcmp(p->getName(), "B") && p->getYearOfBirth() == 1925
+		&& p->getSex() == 'x' && p->getPhoneNumber() == 123456;
+	
+
+	delete p;
+}
+
 Person_test::Person_test(){
-	set_exist = get_exist = set_true_back_value = get_true_back_value = true;
+	exist = false;	
+	true_back_value = false;
+
 }
--- PersonCopyConstructor_CPP_HW3/test_set_get.h
+++ PersonCopyConstructor_CPP_HW3/test_set_get.h
@@ -1,17 +1,16 @@
 #pragma once
+
 #ifndef TEST_SET_GET_H
 #define TEST_SET_GET_H
 class Person_test {
-	bool set_exist;
-	bool get_exist;
-	bool set_true_back_value;
-	bool get_true_back_value;
+	bool exist;
+	bool true_back_value;
 public:
-	void setExist();
-	void getExist();
-	void setTrueBackValue();
-	void getTrueBackValue();
+	void test();
+	
+	
 	void exeAllTest();
 	Person_test();
+	
 };
 #endif
